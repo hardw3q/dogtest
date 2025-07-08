685bec8b65dedeb2169c940fb457bea232c928a7
hardw3q <sob.dod@gmail.com>
2025-07-08 14:52:44 +0300

    linter

--- src/randomdog/randomdog.service.ts
+++ src/randomdog/randomdog.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import {ConfigService} from "@nestjs/config";
-import axios, {AxiosInstance} from "axios";
+import { ConfigService } from '@nestjs/config';
+import axios, { AxiosInstance } from 'axios';
 
 @Injectable()
 export class RandomdogService {
-    private randomInstance: AxiosInstance;
+  private randomInstance: AxiosInstance;
 
-    constructor(private readonly configService: ConfigService) {
-        const baseURL = this.configService.get<string>('DOGTEST_URL');
-        const defaultConfig = {
-            baseURL,
-        };
-        this.randomInstance = axios.create(defaultConfig);
-    }
-    async getAllDogs() {
-        const dogs = await this.randomInstance.get('/doggos');
-        return dogs.data
-    }
+  constructor(private readonly configService: ConfigService) {
+    const baseURL = this.configService.get<string>('DOGTEST_URL');
+    const defaultConfig = {
+      baseURL,
+    };
+    this.randomInstance = axios.create(defaultConfig);
+  }
+  async getAllDogs() {
+    const dogs = await this.randomInstance.get('/doggos');
+    return dogs.data;
+  }
 }
